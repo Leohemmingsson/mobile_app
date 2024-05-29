a239daf3a6fc3f5aeba3c45a9224d7e24a80d579
suahus-01 <suad.huseynli@rocksigma.com>
2024-05-29 19:15:21 +0200

    i am about commit haram

---
+++ app/src/main/java/com/ltu/m7019e/mobile_app/database/UserDao.kt
@@ -0,0 +1,20 @@
+package com.ltu.m7019e.mobile_app.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ltu.m7019e.mobile_app.model.User
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM users WHERE username = :username AND password = :password")
+    suspend fun getUser(username: String, password: String): User?
+
+    @Query("SELECT * FROM users WHERE username = :username")
+    suspend fun getUserByUsername(username: String): User?
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(user: User)
+
+
+}
---
+++ app/src/main/java/com/ltu/m7019e/mobile_app/model/User.kt
@@ -0,0 +1,13 @@
+package com.ltu.m7019e.mobile_app.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val username: String,
+    val password: String,
+    val country: String
+)
