bece587ea393dd744e2737cc4c87ebad2b8cce64
sanlju <sanlju-9@student.ltu.se>
2024-05-30 13:49:30 +0200

    password fix

--- app/src/main/java/com/ltu/m7019e/mobile_app/database/UserRepository.kt
+++ app/src/main/java/com/ltu/m7019e/mobile_app/database/UserRepository.kt
@@ -6,11 +6,9 @@ class UserRepository(private val userDao: UserDao) {
 
 
     suspend fun login(username: String, password: String): Boolean {
-        val user = userDao.getUserByUsername(username)
+        val user = userDao.getUser(username, password)
         return user != null
     }
-
-
 
     suspend fun register(user: User) {
         userDao.insertUser(user)
